160b8a495110d559e4a7cc5cb6676785d5f249dd
saugat97 <saugat.thapa97@gmail.com>
2019-10-18 22:44:01 -0500

    Github App

---
+++ Github App/Github App/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using GithubApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GithubApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var url = "https://api.github.com/search/repositories?q=CSharp&sort=stars&order=desc";
+
+            using (var webClient = new WebClient())
+            {
+                webClient.Headers.Add("user-agent", "Only a test!");
+                var rawJSON = webClient.DownloadString(url);
+                var repositories = JsonConvert.DeserializeObject<Repositories>(rawJSON);
+
+                return View(repositories);
+            }
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ Github App/Github App/Models/Repo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GithubApp.Models
+{
+    public class Repo
+    {
+        public int Id { get; set; }
+        public string Node_id { get; set; }
+        public string Name { get; set; }
+        public string Full_name { get; set; }
+        public bool Private { get; set; }
+        public string Html_url { get; set; }
+        public string Description { get; set; }
+        public string Stargazers_count { get; set; }
+        public string Created_at { get; set; }
+        public string Updated_at { get; set; }
+        public string Git_url { get; set; }
+        public string Ssh_url { get; set; }
+        public Owner Owner { get; set; }
+
+    }
+
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Url { get; set; }
+
+    }
+
+    public class Repositories
+    {
+        public int Total_count { get; set; }
+        public bool Incomplete_results { get; set; }
+        public List<Repo> Items { get; set; }
+    }
+}
